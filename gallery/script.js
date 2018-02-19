ac5690d18cb56f11d43fe86b7641ea4901342ede
Dylan V <dylanvanelli@gmail.com>
2018-02-19 17:43:43 -0500

    updates to image gallery

--- gallery/script.js
+++ gallery/script.js
@@ -1,101 +1,68 @@
-//var jsonFile;
-//var images = [];
-//var categories = [];
 
-$(document).ready(function () {
-    checkFunc();
-    $(document).on('click', '#nature', function () { 
-    console.log("REMOVING FOOD PICS, JUST BUILDINGS");
-    var checkBoxNature = document.getElementById("myCheckNature");
-    $("#fun").empty();
-    $('#myCheckNature').prop('checked', true);
-    $('#myCheckFood').prop('checked', false);
 
-    if (checkBoxNature.checked === true) {
-        loadImages('pics.json');
-    }
+$(document).ready(function(){
+    isReady();
+    //Check both check boxes
+    $(document).on("click", "#nature", function(){
+    $("#isNatureChecked").prop("checked", true);
+    $("#isBuildingChecked").prop("checked", false);
+    });
+
+    $(document).on("click","#buildings",function(){
+        $("#isBuildingChecked").prop("checked", true);
+        $("#isNatureChecked").prop("checked", false);
+    });
 });
 
-$(document).on('click', '#buildings', function() { 
-    console.log("REMOVING NATURE PICS, JUST FOOD");
-    var checkBoxFood = document.getElementById("myCheckFood");
-    $("#fun").empty();
-    $('#myCheckFood').prop('checked', true);
-    $('#myCheckNature').prop('checked', false);
-
-    if(checkBoxFood.checked === true){
-        loadImages('food.json');
+function isReady(){
+    // Why can"t these be in the jQuery format? Something about strict equality
+    var isBuildingChecked = document.getElementById("isBuildingChecked");
+    var isNatureChecked = document.getElementById("isNatureChecked");
+    //Clear images from unchecked category
+    $("#loadImages").empty();
+    if(isBuildingChecked.checked === true){
+        loadImages("buildingPics.json");
     }
-});
-              });
-
-function modal(image){
-    console.log("AT MODAL");
-    console.log(image);
-    $('#myModal').css("display","block");
-    $("#modal-image").attr('src', image.attr('src')).attr('alt', image.attr('alt'));
-    $("#caption").html(image.attr('alt'));
-    console.log("DONE WITH MODAL");
-}
-console.log($('.close'));
-$('.close').click(function(){
-    console.log("CLICKED TO CLOSE");
-    $('#myModal').css("this.style.display","none");
-});
-function showDivs(n) {
-console.log("showDivs");
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-    console.log(x);
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-      console.log(x[i]);
-      console.log("hi");
-     x[i].style.display = "none"; 
-  }
-    if(x.length>0){
-        x[slideIndex-1].style.display = "block";  
-    }
-    console.log(x);
-    console.log(x[slideIndex-1]);
-}
-    
-function checkFunc(){
-    var checkBoxFood = document.getElementById("myCheckFood");
-    var checkBoxNature = document.getElementById("myCheckNature");
-    $("#fun").empty();
-    if(checkBoxFood.checked === true){
-        loadImages('food.json');
-    }
-    if (checkBoxNature.checked === true){
-        loadImages('pics.json');
+    if (isNatureChecked.checked === true){
+        loadImages("naturePics.json");
     } 
 }
-
-function loadImages(jsonFile){
-         $.getJSON(jsonFile, function(data) {
-             var divTag = $('#fun');
-             var category = 'mySlides';
-             //var id;
-
-        $.each(data.images, function() {
-            //console.log();
-            //console.log(e.url);
-            console.log(this.url);
-            console.log("LOADING IN IMAGES");
-            $('<img>').addClass(category).appendTo(divTag).attr('src', this.url).attr('alt',this.caption).attr('style','width:100%').attr('id',this.category).click(function(){
-                modal($(this));
-            });
+//Load into loadImages div images of specific category (specified in isReady())
+function loadImages(file){
+        $.getJSON(file, function(data){
+        $.each(data.images, function(){
+          //Append each cooresponding image to the loadImages container
+          $("<img>").addClass("mySlides").appendTo($("#loadImages")).attr("src", this.src).attr("id",this.category).attr("alt",this.caption).click(function(){
+                formatModal($(this));
+          });
         });
-     });
+        });
 }
 
+//Format modal images
+function formatModal(image){
+    $("#modalDiv").css("display","block");
+    $("#modalImage").attr("src", image.attr("src")).attr("alt", image.attr("alt"));
+    $("#modalCaption").html(image.attr("alt"));
+}
+//Written using WC3 slideshow tutorial
+function getDivs(n){
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }    
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (var i=0; i<slides.length; i++){
+    slides[i].style.display = "none"; 
+  }
+  if(slides.length>0){
+    slides[slideIndex-1].style.display = "block";  
+  }
+}
 
 var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    console.log("plusDivs");
-  showDivs(slideIndex += n);
+function addDivs(n){
+  getDivs(slideIndex += n);
 }
