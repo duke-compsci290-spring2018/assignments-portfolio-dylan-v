b554783853e222c4f2968c50fa4f10631f363b61
Dylan V <dylanvanelli@gmail.com>
2018-02-20 13:37:08 -0500

    final commit

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -2,34 +2,19 @@
 $(document).ready(function(){
     console.log("document ready!")
     checkButtons();
-    loadImgFiles("buildingPics.json");
-    loadImgFiles("naturePics.json");
-    //Check if nature box is checked
-    $(document).on("click", "#nature", function(){
-      $("#natureCheckbox").prop("checked", true);
-      $("#cityCheckbox").prop("checked", false);
-    });
-    //Check if city box is checked
-    $(document).on("click","#buildings",function(){
-      $("#natureCheckbox").prop("checked", false);
-      $("#cityCheckbox").prop("checked", true);
-    });
 });
 //Load images of specific tag
 function checkButtons(){
-    // Why can"t these be in the jQuery format? Something about strict equality
-    var cityCheckbox = document.getElementById("cityCheckbox");
-    var natureCheckbox = document.getElementById("natureCheckbox");
     console.log(cityCheckbox);
     console.log(natureCheckbox);
-    //Clear images from unchecked category
+    //Clear images before checking
     $("#galleryImg").empty();
     $("#compiledImg").empty();
-    if(cityCheckbox.checked === true){
+    if(document.getElementById("cityCheckbox").checked === true){
         console.log("loading city images");
         loadImgFiles("buildingPics.json");
     }
-    if (natureCheckbox.checked === true){
+    if (document.getElementById("natureCheckbox").checked === true){
         console.log("loading nature images");
         loadImgFiles("naturePics.json");
     } 
@@ -38,23 +23,24 @@ function checkButtons(){
 function loadImgFiles(file){
         $.getJSON(file, function(data){
           $.each(data.images, function(){
-            //Append each cooresponding image to the loadImgFiles container
-            $("<img>").addClass("slideshow").appendTo($("#galleryImg")).attr("src", this.src).attr("id",this.tag).attr("alt",this.caption).click(function(){
-              formatModal($(this));
+            //Load images into gallery div
+            $("<img>").attr("src", this.src).attr("id",this.tag).attr("alt",this.caption).addClass("slideshow").appendTo($("#galleryImg")).click(function(){
+              //Format cooresponding modal img
+              $("#modalCaption").html($(this).attr("alt"));
+              $("#modalDiv").css("display","block");
+              $("#modalImage").attr("src", $(this).attr("src")).attr("alt", $(this).attr("alt"));
               console.log(this.src);
             });
-            $("<img>").appendTo($("#compiledImg")).attr("src", this.src).attr("id",this.tag).attr("alt",this.caption).click(function(){
-              formatModal($(this));
+            //Load images into compiled div
+            $("<img>").attr("src", this.src).attr("id",this.tag).attr("alt",this.caption).appendTo($("#compiledImg")).click(function(){
+              //Format cooresponding modal img
+              $("#modalCaption").html($(this).attr("alt"));
+              $("#modalDiv").css("display","block");
+              $("#modalImage").attr("src", $(this).attr("src")).attr("alt", $(this).attr("alt"));
               console.log(this.src);
             });
           });
         });
-}
-//Apply attributes and css to modal images (when a picture is clicked and enters lightbox mode)
-function formatModal(image){
-    $("#modalCaption").html(image.attr("alt"));
-    $("#modalDiv").css("display","block");
-    $("#modalImage").attr("src", image.attr("src")).attr("alt", image.attr("alt"));
 }
 //getDivs and addDivs written with help of WC3 slideshow tutorial
 function getDivs(n){
